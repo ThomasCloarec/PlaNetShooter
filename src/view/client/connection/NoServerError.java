ea82c421096521a8bd1271c64b6825ad0c61784f
ThomasCloarec <thomas.cloarec.d@gmail.com>
2019-01-04 14:11:29 +0100

    Improve GameMenuPanel and finish to revert unwanted changes

--- src/view/client/connection/NoServerError.java
+++ src/view/client/connection/NoServerError.java
@@ -5,6 +5,6 @@ import javax.swing.*;
 public class NoServerError extends JOptionPane {
     public NoServerError() {
         super();
-        showMessageDialog(null, "Please launch a game_frame server before !", "No server found", JOptionPane.ERROR_MESSAGE);
+        showMessageDialog(null, "Please launch a game server before !", "No server found", JOptionPane.ERROR_MESSAGE);
     }
 }
--- src/view/client/game_frame/menu/GameMenuPanel.java
+++ src/view/client/game_frame/menu/GameMenuPanel.java
@@ -4,13 +4,30 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameMenuPanel extends JPanel {
+    private JButton jButton = new JButton("Button");
+
+    // TODO : Change the GameMenuPanel by some background images for the panel and the buttons
     public GameMenuPanel() {
         super();
-        this.setBackground(Color.gray);
-        this.setLayout(new GridLayout(1,5));
-        this.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.black));
-        this.add(new JButton("Button"));
-        for (int i = 0; i < 4 ; i++ )
+        this.setLayout(new GridLayout(1, 5));
+        jButton.setFocusable(false);
+        jButton.setBorder(BorderFactory.createMatteBorder(1,1,1,1,Color.darkGray));
+        jButton.setBackground(new Color(150,150,150));
+        this.add(jButton);
+        for (int i = 0; i < 4; i++)
             this.add(new JLabel());
+        this.setBorder(BorderFactory.createMatteBorder(4, 8, 4, 8, Color.lightGray));
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D graphics = (Graphics2D) g;
+        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        graphics.setColor(Color.gray);
+        graphics.fillRoundRect(2, 2, this.getWidth()-4, this.getHeight()-4, 15, 15);//paint background
+        graphics.setColor(Color.black);
+        graphics.drawRoundRect(2, 2, this.getWidth()-4, this.getHeight()-4, 15, 15);//paint border
+        jButton.repaint();
     }
 }
