6d84fdfeeed67a3446e296edbf4efceb929742f5
ThomasCloarec <thomas.cloarec.d@gmail.com>
2019-01-05 23:57:26 +0100

    Bring back alignment in MainClient class

--- src/MainClient.java
+++ src/MainClient.java
@@ -30,98 +30,98 @@ private static CharacterView characterView;
 private static final String MOVE_LEFT = "move left", MOVE_RIGHT = "move right", MOVE_STOP = "move stop";
 private static final int FPS = 60;
 
-public static void main(String[] args) {
-    // If a game server is up on the network
-    if (new Client().discoverHost(Network.getUdpPort(), 5000) != null) {
-        launchGameClient();
+    public static void main(String[] args) {
+        // If a game server is up on the network
+        if (new Client().discoverHost(Network.getUdpPort(), 5000) != null) {
+            launchGameClient();
 
-        SwingUtilities.invokeLater(() -> {
-            gameFrame = new GameFrame(clientName);
-            gameFrame.addWindowListener(new WindowAdapter() {
-                public void windowClosing(WindowEvent evt) {
-                    System.out.println("You are disconnected !");
-                    System.exit(0);
-                }
-            });
-
-            Platform[] platforms = new Platform[Platform.getPlatformNumber()];
-            gameFrame.getGamePanel().setPlatformsView(new PlatformView[Platform.getPlatformNumber()]);
-            for (int i = 0; i < Platform.getPlatformNumber(); i++) {
-                platforms[i] = new Platform();
-                gameFrame.getGamePanel().setEachPlatformView(i, new PlatformView(
-                        platforms[i].getRelativeX(),
-                        platforms[i].getRelativeY(),
-                        Platform.getRelativeWidth(),
-                        Platform.getRelativeHeight()));
-
-                allSolidObjects.add(platforms[i]);
-            }
-
-            character = new Character();
-            characterView = new CharacterView(
-                    character.getRelativeX(),
-                    character.getRelativeY(),
-                    Character.getRelativeWidth(),
-                    Character.getRelativeHeight());
-
-            gameFrame.getGamePanel().setCharacterView(characterView);
-
-            final InputMap IM = gameFrame.getGamePanel().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-            final ActionMap AM = gameFrame.getGamePanel().getActionMap();
-            MovementState movementState = new MovementState();
-
-            IM.put(KeyStroke.getKeyStroke(KeyEvent.VK_Q, 0, true), MOVE_STOP);
-            IM.put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0, true), MOVE_STOP);
-            IM.put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, true), MOVE_STOP);
-            IM.put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0, true), MOVE_STOP);
-            AM.put(MOVE_STOP, new MoveXAction(movementState, 0f));
-
-            IM.put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, false), MOVE_RIGHT);
-            IM.put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0, false), MOVE_RIGHT);
-            AM.put(MOVE_RIGHT, new MoveXAction(movementState, Character.getRelativeSpeed()));
-
-            IM.put(KeyStroke.getKeyStroke(KeyEvent.VK_Q, 0, false), MOVE_LEFT);
-            IM.put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0, false), MOVE_LEFT);
-            AM.put(MOVE_LEFT, new MoveXAction(movementState, -Character.getRelativeSpeed()));
-            Timer timer = new Timer(1000/FPS, e -> {
-                if (movementState.xDirection < 0) {
-                    stopRight = false;
-                    if (!stopLeft) {
-                        character.setRelativeX(character.getRelativeX() + movementState.xDirection);
-                        for (Object object : allSolidObjects) {
-                            if (CollisionDetection.isCollisionBetween(character, object)) {
-                                stopLeft = true;
-                            }
-                        }
+            SwingUtilities.invokeLater(() -> {
+                gameFrame = new GameFrame(clientName);
+                gameFrame.addWindowListener(new WindowAdapter() {
+                    public void windowClosing(WindowEvent evt) {
+                        System.out.println("You are disconnected !");
+                        System.exit(0);
                     }
-                } else if (movementState.xDirection > 0) {
-                    stopLeft = false;
-                    if (!stopRight) {
-                        character.setRelativeX(character.getRelativeX() + movementState.xDirection);
-                        for (Object object : allSolidObjects) {
-                            if (CollisionDetection.isCollisionBetween(character, object)) {
-                                stopRight = true;
-                            }
-                        }
-                    }
+                });
+
+                Platform[] platforms = new Platform[Platform.getPlatformNumber()];
+                gameFrame.getGamePanel().setPlatformsView(new PlatformView[Platform.getPlatformNumber()]);
+                for (int i = 0; i < Platform.getPlatformNumber(); i++) {
+                    platforms[i] = new Platform();
+                    gameFrame.getGamePanel().setEachPlatformView(i, new PlatformView(
+                            platforms[i].getRelativeX(),
+                            platforms[i].getRelativeY(),
+                            Platform.getRelativeWidth(),
+                            Platform.getRelativeHeight()));
+
+                    allSolidObjects.add(platforms[i]);
                 }
 
-                characterView.setRelativeX(character.getRelativeX());
-                characterView.setRelativeY(character.getRelativeY());
+                character = new Character();
+                characterView = new CharacterView(
+                        character.getRelativeX(),
+                        character.getRelativeY(),
+                        Character.getRelativeWidth(),
+                        Character.getRelativeHeight());
+
                 gameFrame.getGamePanel().setCharacterView(characterView);
 
-                gameFrame.getGamePanel().repaint();
+                final InputMap IM = gameFrame.getGamePanel().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+                final ActionMap AM = gameFrame.getGamePanel().getActionMap();
+                MovementState movementState = new MovementState();
+
+                IM.put(KeyStroke.getKeyStroke(KeyEvent.VK_Q, 0, true), MOVE_STOP);
+                IM.put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0, true), MOVE_STOP);
+                IM.put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, true), MOVE_STOP);
+                IM.put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0, true), MOVE_STOP);
+                AM.put(MOVE_STOP, new MoveXAction(movementState, 0f));
+
+                IM.put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, false), MOVE_RIGHT);
+                IM.put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0, false), MOVE_RIGHT);
+                AM.put(MOVE_RIGHT, new MoveXAction(movementState, Character.getRelativeSpeed()));
+
+                IM.put(KeyStroke.getKeyStroke(KeyEvent.VK_Q, 0, false), MOVE_LEFT);
+                IM.put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0, false), MOVE_LEFT);
+                AM.put(MOVE_LEFT, new MoveXAction(movementState, -Character.getRelativeSpeed()));
+                Timer timer = new Timer(1000/FPS, e -> {
+                    if (movementState.xDirection < 0) {
+                        stopRight = false;
+                        if (!stopLeft) {
+                            character.setRelativeX(character.getRelativeX() + movementState.xDirection);
+                            for (Object object : allSolidObjects) {
+                                if (CollisionDetection.isCollisionBetween(character, object)) {
+                                    stopLeft = true;
+                                }
+                            }
+                        }
+                    } else if (movementState.xDirection > 0) {
+                        stopLeft = false;
+                        if (!stopRight) {
+                            character.setRelativeX(character.getRelativeX() + movementState.xDirection);
+                            for (Object object : allSolidObjects) {
+                                if (CollisionDetection.isCollisionBetween(character, object)) {
+                                    stopRight = true;
+                                }
+                            }
+                        }
+                    }
+
+                    characterView.setRelativeX(character.getRelativeX());
+                    characterView.setRelativeY(character.getRelativeY());
+                    gameFrame.getGamePanel().setCharacterView(characterView);
+
+                    gameFrame.getGamePanel().repaint();
+                });
+                timer.start();
+
             });
-            timer.start();
-
-        });
+        }
+        else {
+            new NoServerError();
+        }
     }
-    else {
-        new NoServerError();
-    }
-}
 
-private static void launchGameClient() {
+    private static void launchGameClient() {
     while(true) {
         try {
             gameClient = new GameClient(AskIPHost.getIPHost());
@@ -158,22 +158,22 @@ private static void launchGameClient() {
     gameClient.connectedListener(clientName);
 }
 
-static class MoveXAction extends AbstractAction {
-    private final MovementState movementState;
-    private final float value;
+    static class MoveXAction extends AbstractAction {
+        private final MovementState movementState;
+        private final float value;
 
-    MoveXAction(MovementState movementState, float value) {
-        this.movementState = movementState;
-        this.value = value;
+        MoveXAction(MovementState movementState, float value) {
+            this.movementState = movementState;
+            this.value = value;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            this.movementState.xDirection = this.value;
+        }
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        this.movementState.xDirection = this.value;
+    static class MovementState {
+        float xDirection;
     }
-}
-
-static class MovementState {
-    float xDirection;
-}
 }
