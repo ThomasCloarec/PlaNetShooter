8f884f471d6bc91449997965bad96c1b82c2cef4
ThomasCloarec <thomas.cloarec.d@gmail.com>
2019-03-15 00:44:47 +0100

    Fix platforms

--- src/model/platforms/Platform.java
+++ src/model/platforms/Platform.java
@@ -10,44 +10,44 @@ public class Platform {
     public Platform() {
         /* HOW THE PLATFORMS ARE :
 
-                 _5_
-            _4_       _7_
-       _2_                 _8_
-          ___3___   ___6___
-       _1_                 _9_
+                 _4_
+            _3_       _6_
+       _1_                 _7_
+          ___2___   ___5___
+       _0_                 _8_
          */
 
-        if (platformLoopCount == 1 || platformLoopCount == 2)
+        if (platformLoopCount == 0 || platformLoopCount == 1)
             this.relativeX = 0f;
-        else if (platformLoopCount == 3)
+        else if (platformLoopCount == 2)
             this.relativeX = 0.222f;
-        else if (platformLoopCount == 4)
+        else if (platformLoopCount == 3)
             this.relativeX = 0.166f;
-        else if (platformLoopCount == 5)
+        else if (platformLoopCount == 4)
             this.relativeX = 0.444f;
-        else if (platformLoopCount == 6)
+        else if (platformLoopCount == 5)
             this.relativeX = 0.555f;
-        else if (platformLoopCount == 7)
+        else if (platformLoopCount == 6)
             this.relativeX = 0.721f;
-        else if (platformLoopCount == 8|| platformLoopCount == 9)
+        else if (platformLoopCount == 7|| platformLoopCount == 8)
             this.relativeX = 0.9f;
 
 
-        if (platformLoopCount == 1 || platformLoopCount == 9)
+        if (platformLoopCount == 0 || platformLoopCount == 8)
             this.relativeY = 0.95f;
-        else if (platformLoopCount == 3 || platformLoopCount == 6)
+        else if (platformLoopCount == 2 || platformLoopCount == 5)
             this.relativeY = 0.75f;
-        else if (platformLoopCount == 2 || platformLoopCount == 8)
+        else if (platformLoopCount == 1 || platformLoopCount == 7)
             this.relativeY = 0.55f;
-        else if (platformLoopCount == 4 || platformLoopCount == 7)
+        else if (platformLoopCount == 3 || platformLoopCount == 6)
             this.relativeY = 0.35f;
-        else if (platformLoopCount == 5)
+        else if (platformLoopCount == 4)
             this.relativeY = 0.25f;
 
-        if (platformLoopCount == 3 || platformLoopCount == 6)
+        if (platformLoopCount == 2 || platformLoopCount == 5)
             this.relativeWidth = 0.222f;
         // in order to move back the platform5 at the center, otherwise it would be pushed on right :
-        if (platformLoopCount == 1)
+        if (platformLoopCount == 0)
             this.relativeX = 0f;
 
         platformLoopCount++;
@@ -71,6 +71,6 @@ public class Platform {
 
     @SuppressWarnings("SameReturnValue")
     public static int getPlatformNumber() {
-        return 12;
+        return 9;
     }
 }
